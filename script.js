83fb77dd62434467322495f752c9c3cb51629a7a
Christopher Phoenix <hello@chrisphoenix.dev>
2026-07-14 14:20:21 -0700

    feat: get computer choice of game move

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 // Rock Paper Scissors game code
-console.log("Hello, world!");
+
+// Yeah I know I shouldn't know about this yet...
+const Choice = Object.freeze({
+  ROCK:     0,
+  PAPER:    1,
+  SCISSORS: 2
+});
+
+
+function getComputerChoice() {
+  let choice = "";
+
+  // generates a random integer between 0 and 2
+  const i = Math.floor( (Math.random() * 3) );
+
+  switch(i) {
+  case Choice.ROCK:
+    choice = "rock";
+    break;
+  case Choice.PAPER:
+    choice = "paper";
+    break;
+  case Choice.SCISSORS:
+    choice = "scissors";
+    break;
+  }
+
+  return choice;
+}
